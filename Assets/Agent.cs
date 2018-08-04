c1a03a183fe5863a3358346aedd0774896424200
Kyle Farnung <kfarnung@outlook.com>
2018-08-04 13:02:32 -0700

    Clean up existing code
    * Ran VS document formatting
    * Added a nodes checked count to each algorithm

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -3,60 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using Priority_Queue;
 
-public class Agent : MonoBehaviour {
+public class Agent : MonoBehaviour
+{
+    IDictionary<Vector3, Vector3> nodeParents = new Dictionary<Vector3, Vector3>();
+    public IDictionary<Vector3, bool> walkablePositions;
+    public IDictionary<Vector3, string> obstacles;
+    IDictionary<Vector3, Sprite> prevSprite = new Dictionary<Vector3, Sprite>();
 
-	IDictionary<Vector3, Vector3> nodeParents = new Dictionary<Vector3, Vector3>();
-	public IDictionary<Vector3, bool> walkablePositions;
-	public IDictionary<Vector3, string> obstacles;
-	IDictionary<Vector3, Sprite> prevSprite = new Dictionary<Vector3, Sprite> ();
+    NodeNetworkCreator nodeNetwork;
+    IList<Vector3> path;
 
-	NodeNetworkCreator nodeNetwork;
-	IList<Vector3> path;
-
-	bool solutionVisible;
-	string prevAlgo;
+    bool solutionVisible;
+    string prevAlgo;
 
     Camera camera;
 
     bool moveCube = false;
-	int i;
+    int i;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         camera = FindObjectOfType<Camera>();
-        nodeNetwork = GameObject.Find ("NodeNetwork").GetComponent<NodeNetworkCreator> ();
-		obstacles = GameObject.Find ("NodeNetwork").GetComponent<NodeNetworkCreator> ().obstacles;
-		walkablePositions = nodeNetwork.walkablePositions;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        nodeNetwork = GameObject.Find("NodeNetwork").GetComponent<NodeNetworkCreator>();
+        obstacles = GameObject.Find("NodeNetwork").GetComponent<NodeNetworkCreator>().obstacles;
+        walkablePositions = nodeNetwork.walkablePositions;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
         camera.transform.position = new Vector3(this.transform.position.x, 10, this.transform.position.z);
 
-		//Hacky way to move the cube along the path.
-		if (moveCube) {
+        //Hacky way to move the cube along the path.
+        if (moveCube)
+        {
             float speed = 25 / Weight(path[i]);
-			float step = Time.deltaTime * speed;
-			transform.position = Vector3.MoveTowards (transform.position, path[i], step);
-			if (transform.position.Equals (path [i]) && i >= 0)
-				i--;
-			if (i < 0)
-				moveCube = false;
-		}
-	}
+            float step = Time.deltaTime * speed;
+            transform.position = Vector3.MoveTowards(transform.position, path[i], step);
+            if (transform.position.Equals(path[i]) && i >= 0)
+                i--;
+            if (i < 0)
+                moveCube = false;
+        }
+    }
 
     int EuclideanEstimate(Vector3 node, Vector3 goal)
     {
-        return (int) Mathf.Sqrt(Mathf.Pow(node.x - goal.x, 2) +
+        return (int)Mathf.Sqrt(Mathf.Pow(node.x - goal.x, 2) +
             Mathf.Pow(node.y - goal.y, 2) +
             Mathf.Pow(node.z - goal.z, 2));
     }
 
     int ManhattanEstimate(Vector3 node, Vector3 goal)
     {
-        return (int) (Mathf.Abs(node.x - goal.x) +
+        return (int)(Mathf.Abs(node.x - goal.x) +
             Mathf.Abs(node.y - goal.y) +
             Mathf.Abs(node.z - goal.z));
     }
@@ -74,9 +75,10 @@ public class Agent : MonoBehaviour {
         return -1;
     }
 
-    Vector3 FindShortestPathAStar(Vector3 startPosition, Vector3 goalPosition, string heuristic) {
-
+    Vector3 FindShortestPathAStar(Vector3 startPosition, Vector3 goalPosition, string heuristic)
+    {
         float timeNow = Time.realtimeSinceStartup;
+        uint checkedNodes = 0;
 
         // A* tries to minimize f(x) = g(x) + h(x), where g(x) is the distance from the start to node "x" and
         //    h(x) is some heuristic that must be admissible, meaning it never overestimates the cost to the next node.
@@ -110,16 +112,18 @@ public class Agent : MonoBehaviour {
         SimplePriorityQueue<Vector3, int> priorityQueue = new SimplePriorityQueue<Vector3, int>();
         priorityQueue.Enqueue(startPosition, heuristicScore[startPosition]);
 
-        while(priorityQueue.Count > 0)
+        while (priorityQueue.Count > 0)
         {
             // Get the node with the least distance from the start
             Vector3 curr = priorityQueue.Dequeue();
+            checkedNodes++;
 
             // If our current node is the goal then stop
             if (curr == goalPosition)
             {
                 print("A*" + heuristic + ": " + distanceFromStart[goalPosition]);
                 print("A*" + heuristic + " time: " + (Time.realtimeSinceStartup - timeNow).ToString());
+                print("A*" + heuristic + " checked: " + checkedNodes);
                 return goalPosition;
             }
 
@@ -162,13 +166,14 @@ public class Agent : MonoBehaviour {
         return startPosition;
     }
 
-	//Dijkstra's algorithm.
-	//Populates IList<Vector3> path with a valid solution to the goalPosition.
-	//Returns the goalPosition if a solution is found.
-	//Returns the startPosition if no solution is found.
-	Vector3 FindShortestPathDijkstra(Vector3 startPosition, Vector3 goalPosition){
-
+    //Dijkstra's algorithm.
+    //Populates IList<Vector3> path with a valid solution to the goalPosition.
+    //Returns the goalPosition if a solution is found.
+    //Returns the startPosition if no solution is found.
+    Vector3 FindShortestPathDijkstra(Vector3 startPosition, Vector3 goalPosition)
+    {
         float timeNow = Time.realtimeSinceStartup;
+        uint checkedNodes = 0;
 
         HashSet<Vector3> exploredNodes = new HashSet<Vector3>();
 
@@ -179,26 +184,29 @@ public class Agent : MonoBehaviour {
         IDictionary<Vector3, int> distances = walkablePositions
             .Where(x => x.Value == true)
             .ToDictionary(x => x.Key, x => int.MaxValue);
-        
+
         //The parent of all nodes is "blank" ((-1, -1, -1) is invalid for our environment)
-        foreach (Vector3 vertex in distances.Keys.ToList()) {
-			nodeParents.Add (new KeyValuePair<Vector3, Vector3> (vertex, new Vector3(-1, -1, -1)));
+        foreach (Vector3 vertex in distances.Keys.ToList())
+        {
+            nodeParents.Add(new KeyValuePair<Vector3, Vector3>(vertex, new Vector3(-1, -1, -1)));
         }
 
         //Our distance from the start to itself is 0
         distances[startPosition] = 0;
         priority.Enqueue(startPosition, 0);
 
-        while (priority.Count > 0) {
-
+        while (priority.Count > 0)
+        {
             Vector3 curr = priority.Dequeue();
+            checkedNodes++;
 
             exploredNodes.Add(curr);
 
-			IList<Vector3> nodes = GetWalkableNodes (curr);
+            IList<Vector3> nodes = GetWalkableNodes(curr);
 
-			//Look at each neighbor to the node
-			foreach (Vector3 node in nodes) {
+            //Look at each neighbor to the node
+            foreach (Vector3 node in nodes)
+            {
                 if (exploredNodes.Contains(node))
                 {
                     continue;
@@ -214,129 +222,153 @@ public class Agent : MonoBehaviour {
                 //If the distance to the parent, PLUS the distance added by the neighbor,
                 //is less than the current distance to the neighbor,
                 //update the neighbor's paent to curr, update its current best distance
-                if (dist < distances [node]) {
-					distances [node] = dist;
-					nodeParents [node] = curr;
-				}
-			}
-		}
+                if (dist < distances[node])
+                {
+                    distances[node] = dist;
+                    nodeParents[node] = curr;
+                }
+            }
+        }
 
         print("Dijkstra: " + distances[goalPosition]);
         print("Dijkstra time: " + (Time.realtimeSinceStartup - timeNow).ToString());
+        print("Dijkstra checked: " + checkedNodes);
 
         return goalPosition;
-	}
+    }
 
-	int Weight(Vector3 node) {
-		if (obstacles.Keys.Contains(node)) {
-			if (obstacles [node] == "slow") {
-				return 3;
-			} else if (obstacles [node] == "verySlow") {
-				return 5;
-			} else {
-				return 1;
-			}
-		} else {
-			return 1;
-		}
-	}
+    int Weight(Vector3 node)
+    {
+        if (obstacles.Keys.Contains(node))
+        {
+            if (obstacles[node] == "slow")
+            {
+                return 3;
+            }
+            else if (obstacles[node] == "verySlow")
+            {
+                return 5;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        else
+        {
+            return 1;
+        }
+    }
 
-	//Breadth first search of graph.
-	//Populates IList<Vector3> path with a valid solution to the goalPosition.
-	//Returns the goalPosition if a solution is found.
-	//Returns the startPosition if no solution is found.
-	Vector3 FindShortestPathBFS(Vector3 startPosition, Vector3 goalPosition){
-		Queue<Vector3> queue = new Queue<Vector3> ();
-		HashSet<Vector3> exploredNodes = new HashSet<Vector3> ();
-		queue.Enqueue (startPosition);
+    //Breadth first search of graph.
+    //Populates IList<Vector3> path with a valid solution to the goalPosition.
+    //Returns the goalPosition if a solution is found.
+    //Returns the startPosition if no solution is found.
+    Vector3 FindShortestPathBFS(Vector3 startPosition, Vector3 goalPosition)
+    {
+        Queue<Vector3> queue = new Queue<Vector3>();
+        HashSet<Vector3> exploredNodes = new HashSet<Vector3>();
+        queue.Enqueue(startPosition);
 
-		while (queue.Count != 0) {
-			Vector3 currentNode = queue.Dequeue ();
-			if (currentNode == goalPosition) {
-				return currentNode;
-			}
+        while (queue.Count != 0)
+        {
+            Vector3 currentNode = queue.Dequeue();
+            if (currentNode == goalPosition)
+            {
+                return currentNode;
+            }
 
-			IList<Vector3> nodes = GetWalkableNodes (currentNode);
+            IList<Vector3> nodes = GetWalkableNodes(currentNode);
 
-			foreach(Vector3 node in nodes){
-				if(!exploredNodes.Contains(node)) {
-					//Mark the node as explored
-					exploredNodes.Add(node);
+            foreach (Vector3 node in nodes)
+            {
+                if (!exploredNodes.Contains(node))
+                {
+                    //Mark the node as explored
+                    exploredNodes.Add(node);
 
-					//Store a reference to the previous node
-					nodeParents.Add (node, currentNode);
+                    //Store a reference to the previous node
+                    nodeParents.Add(node, currentNode);
 
-					//Add this to the queue of nodes to examine
-					queue.Enqueue (node);
-				}
-			}
-		}
+                    //Add this to the queue of nodes to examine
+                    queue.Enqueue(node);
+                }
+            }
+        }
 
-		return startPosition;
-	}
+        return startPosition;
+    }
 
-	//Depth first search of graph.
-	//Populates IList<Vector3> path with a valid solution to the goalPosition.
-	//Returns the goalPosition if a solution is found.
-	//Returns the startPosition if no solution is found.
-	Vector3 FindShortestPathDFS(Vector3 startPosition, Vector3 goalPosition){
-		Stack<Vector3> stack = new Stack<Vector3> ();
-		HashSet<Vector3> exploredNodes = new HashSet<Vector3> ();
-		stack.Push (startPosition);
+    //Depth first search of graph.
+    //Populates IList<Vector3> path with a valid solution to the goalPosition.
+    //Returns the goalPosition if a solution is found.
+    //Returns the startPosition if no solution is found.
+    Vector3 FindShortestPathDFS(Vector3 startPosition, Vector3 goalPosition)
+    {
+        Stack<Vector3> stack = new Stack<Vector3>();
+        HashSet<Vector3> exploredNodes = new HashSet<Vector3>();
+        stack.Push(startPosition);
 
-		while (stack.Count != 0) {
-			Vector3 currentNode = stack.Pop ();
-			if (currentNode == goalPosition) {
-				return currentNode;
-			}
+        while (stack.Count != 0)
+        {
+            Vector3 currentNode = stack.Pop();
+            if (currentNode == goalPosition)
+            {
+                return currentNode;
+            }
 
-			IList<Vector3> nodes = GetWalkableNodes (currentNode);
+            IList<Vector3> nodes = GetWalkableNodes(currentNode);
 
-			foreach(Vector3 node in nodes){
-				if(!exploredNodes.Contains(node)) {
-					//Mark the node as explored
-					exploredNodes.Add(node);
+            foreach (Vector3 node in nodes)
+            {
+                if (!exploredNodes.Contains(node))
+                {
+                    //Mark the node as explored
+                    exploredNodes.Add(node);
 
-					//Store a reference to the previous node
-					nodeParents.Add (node, currentNode);
+                    //Store a reference to the previous node
+                    nodeParents.Add(node, currentNode);
 
-					//Add this to the queue of nodes to examine
-					stack.Push (node);
-				}
-			}
-		}
+                    //Add this to the queue of nodes to examine
+                    stack.Push(node);
+                }
+            }
+        }
 
-		return startPosition;
-	}
+        return startPosition;
+    }
 
-	bool CanMove(Vector3 nextPosition) {
-		return (walkablePositions.ContainsKey (nextPosition) ? walkablePositions [nextPosition] : false);
-	}
+    bool CanMove(Vector3 nextPosition)
+    {
+        return (walkablePositions.ContainsKey(nextPosition) ? walkablePositions[nextPosition] : false);
+    }
 
-	public void DisplayShortestPath(string algorithm) {
+    public void DisplayShortestPath(string algorithm)
+    {
+        if (solutionVisible && algorithm == prevAlgo)
+        {
+            foreach (Vector3 node in path)
+            {
+                nodeNetwork.nodeReference[node].GetComponent<SpriteRenderer>().sprite = prevSprite[node];
+            }
 
-		if (solutionVisible && algorithm == prevAlgo) {
-			foreach (Vector3 node in path) {
-				nodeNetwork.nodeReference [node].GetComponent<SpriteRenderer> ().sprite = prevSprite[node];
-			}
+            solutionVisible = false;
+            return;
+        }
 
-			solutionVisible = false;
-			return;
-		}
-			
-		nodeParents = new Dictionary<Vector3, Vector3>();
-		path = FindShortestPath (algorithm);
+        nodeParents = new Dictionary<Vector3, Vector3>();
+        path = FindShortestPath(algorithm);
 
-		if (path == null)
-			return;
+        if (path == null)
+            return;
 
-		Sprite exploredTile = Resources.Load <Sprite>("path 1");
-		Sprite victoryTile = Resources.Load<Sprite> ("victory 1");
-		Sprite dijkstraTile = Resources.Load<Sprite> ("dijkstra");
+        Sprite exploredTile = Resources.Load<Sprite>("path 1");
+        Sprite victoryTile = Resources.Load<Sprite>("victory 1");
+        Sprite dijkstraTile = Resources.Load<Sprite>("dijkstra");
 
-		foreach (Vector3 node in path) {
-			
-			prevSprite[node] = nodeNetwork.nodeReference [node].GetComponent<SpriteRenderer> ().sprite;
+        foreach (Vector3 node in path)
+        {
+            prevSprite[node] = nodeNetwork.nodeReference[node].GetComponent<SpriteRenderer>().sprite;
 
             if (algorithm == "DFS")
             {
@@ -358,24 +390,25 @@ public class Agent : MonoBehaviour {
             {
                 nodeNetwork.nodeReference[node].GetComponent<SpriteRenderer>().sprite = dijkstraTile;
             }
-		}
+        }
 
-		nodeNetwork.nodeReference [path [0]].GetComponent<SpriteRenderer> ().sprite = victoryTile;
+        nodeNetwork.nodeReference[path[0]].GetComponent<SpriteRenderer>().sprite = victoryTile;
 
-		i = path.Count - 1;
+        i = path.Count - 1;
 
-		solutionVisible = true;
-		prevAlgo = algorithm;
-	}
+        solutionVisible = true;
+        prevAlgo = algorithm;
+    }
 
-	public void MoveCube(){
-		moveCube = true;
-	}
+    public void MoveCube()
+    {
+        moveCube = true;
+    }
 
-	IList<Vector3> FindShortestPath(string algorithm){
-
-		IList<Vector3> path = new List<Vector3> ();
-		Vector3 goal;
+    IList<Vector3> FindShortestPath(string algorithm)
+    {
+        IList<Vector3> path = new List<Vector3>();
+        Vector3 goal;
         if (algorithm == "DFS")
         {
             goal = FindShortestPathDFS(this.transform.localPosition, GameObject.Find("Goal").transform.localPosition);
@@ -397,41 +430,45 @@ public class Agent : MonoBehaviour {
             goal = FindShortestPathDijkstra(this.transform.localPosition, GameObject.Find("Goal").transform.localPosition);
         }
 
-		if (goal == this.transform.localPosition || !nodeParents.ContainsKey(nodeParents[goal])) {
-			//No solution was found.
-			return null;
-		}
+        if (goal == this.transform.localPosition || !nodeParents.ContainsKey(nodeParents[goal]))
+        {
+            //No solution was found.
+            return null;
+        }
 
-		Vector3 curr = goal;
-		while (curr != this.transform.localPosition) {
-			path.Add (curr);
-			curr = nodeParents [curr];
-		}
+        Vector3 curr = goal;
+        while (curr != this.transform.localPosition)
+        {
+            path.Add(curr);
+            curr = nodeParents[curr];
+        }
 
-		return path;
-	}
+        return path;
+    }
 
-	IList<Vector3> GetWalkableNodes(Vector3 curr) {
+    IList<Vector3> GetWalkableNodes(Vector3 curr)
+    {
+        IList<Vector3> walkableNodes = new List<Vector3>();
 
-		IList<Vector3> walkableNodes = new List<Vector3> ();
-
-		IList<Vector3> possibleNodes = new List<Vector3> () {
-			new Vector3 (curr.x + 1, curr.y, curr.z),
-			new Vector3 (curr.x - 1, curr.y, curr.z),
-			new Vector3 (curr.x, curr.y, curr.z + 1),
-			new Vector3 (curr.x, curr.y, curr.z - 1),
+        IList<Vector3> possibleNodes = new List<Vector3>() {
+            new Vector3 (curr.x + 1, curr.y, curr.z),
+            new Vector3 (curr.x - 1, curr.y, curr.z),
+            new Vector3 (curr.x, curr.y, curr.z + 1),
+            new Vector3 (curr.x, curr.y, curr.z - 1),
             new Vector3 (curr.x + 1, curr.y, curr.z + 1),
             new Vector3 (curr.x + 1, curr.y, curr.z - 1),
             new Vector3 (curr.x - 1, curr.y, curr.z + 1),
             new Vector3 (curr.x - 1, curr.y, curr.z - 1)
         };
 
-		foreach (Vector3 node in possibleNodes) {
-			if (CanMove (node)) {
-				walkableNodes.Add (node);
-			} 
-		}
+        foreach (Vector3 node in possibleNodes)
+        {
+            if (CanMove(node))
+            {
+                walkableNodes.Add(node);
+            }
+        }
 
-		return walkableNodes;
-	}
+        return walkableNodes;
+    }
 }
